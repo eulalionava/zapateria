44aad6bfe9b3fd6cfd8dd69845da80d70213858e
eulalionava <eulalio_cruz_nava@hotmail.com>
2026-03-15 21:20:42 -0600

    Texto de rango de tallas al seleccionar el filtro de categoria

--- src/components/filterGender.tsx
+++ src/components/filterGender.tsx
@@ -9,79 +9,93 @@ export const FilterGender = () => {
     const { changeFilter} = useProductStore(state=>state)
     const [filterForGender,setFilterForGender] = useState('all')
 
+    const rangesOfSizes: { [key: string]: string } = {
+        all:'',
+        men:'Tallas desde 23 hasta 27',
+        woman:'Tallas desde 23 hasta 27',
+        girl:'Tallas desde 15 hasta 22',
+        boy:'Tallas desde 15 hasta 22',
+        unisex:'Tallas Unisex'
+    }
+
     const OnSelect = (gender:string)=>{
         setFilterForGender(gender)
         changeFilter(gender)
     }
 
     return (
-        <section className="flex gap-2 flex-wrap p-3">
-            <div 
-                onClick={() => OnSelect('all')}
-                className={
-                    clsx(
-                        "text-center rounded-md w-[100px] p-1",
-                        { 
-                            'bg-amber-600 text-white': filterForGender === 'all',
-                            'bg-white text-gray-300': filterForGender !== 'all'
-                        }
-                    )
-            }>Todos</div>
-            <div 
-                onClick={() => OnSelect('men')}
-                className={
-                    clsx(
-                        "text-center rounded-md w-[100px] p-1",
-                        { 
-                            'bg-amber-600 text-white': filterForGender === 'men',
-                            'bg-white text-gray-300': filterForGender !== 'men'
-                        }
-                    )
-            }>Hombre</div>
-            <div 
-                onClick={() => OnSelect('woman')}
-                className={
-                    clsx(
-                        "text-center rounded-md w-[100px] p-1",
-                        { 
-                            'bg-amber-600 text-white': filterForGender === 'woman',
-                            'bg-white text-gray-300': filterForGender !== 'woman'
-                        }
-                    )
-            }>Mujer</div>
-            <div 
-                onClick={() => OnSelect('girl')}
-                className={
-                    clsx(
-                        "text-center rounded-md w-[100px] p-1",
-                        { 
-                            'bg-amber-600 text-white': filterForGender === 'girl',
-                            'bg-white text-gray-300': filterForGender !== 'girl'
-                        }
-                    )
-            }>Niña</div>
-            <div 
-                onClick={() => OnSelect('boy')}
-                className={
-                    clsx(
-                        "text-center rounded-md w-[100px] p-1",
-                        { 
-                            'bg-amber-600 text-white': filterForGender === 'boy',
-                            'bg-white text-gray-300': filterForGender !== 'boy'
-                        }
-                    )
-            }>Niño</div>
-            <div 
-                onClick={() => OnSelect('unisex')}
-                className={
-                    clsx(
-                        "text-center rounded-md w-[100px] p-1",
-                        { 
-                            'bg-amber-600 text-white': filterForGender === 'unisex',
-                            'bg-white text-gray-300': filterForGender !== 'unisex'
-                        }
-                    )
-            }>Unisex</div>
-        </section>
+        <>
+            <section className="flex gap-2 flex-wrap p-3">
+                <div 
+                    onClick={() => OnSelect('all')}
+                    className={
+                        clsx(
+                            "text-center rounded-md w-[100px] p-1",
+                            { 
+                                'bg-amber-600 text-white': filterForGender === 'all',
+                                'bg-white text-gray-300': filterForGender !== 'all'
+                            }
+                        )
+                }>Todos</div>
+                <div 
+                    onClick={() => OnSelect('men')}
+                    className={
+                        clsx(
+                            "text-center rounded-md w-[100px] p-1",
+                            { 
+                                'bg-amber-600 text-white': filterForGender === 'men',
+                                'bg-white text-gray-300': filterForGender !== 'men'
+                            }
+                        )
+                }>Hombre</div>
+                <div 
+                    onClick={() => OnSelect('woman')}
+                    className={
+                        clsx(
+                            "text-center rounded-md w-[100px] p-1",
+                            { 
+                                'bg-amber-600 text-white': filterForGender === 'woman',
+                                'bg-white text-gray-300': filterForGender !== 'woman'
+                            }
+                        )
+                }>Mujer</div>
+                <div 
+                    onClick={() => OnSelect('girl')}
+                    className={
+                        clsx(
+                            "text-center rounded-md w-[100px] p-1",
+                            { 
+                                'bg-amber-600 text-white': filterForGender === 'girl',
+                                'bg-white text-gray-300': filterForGender !== 'girl'
+                            }
+                        )
+                }>Niña</div>
+                <div 
+                    onClick={() => OnSelect('boy')}
+                    className={
+                        clsx(
+                            "text-center rounded-md w-[100px] p-1",
+                            { 
+                                'bg-amber-600 text-white': filterForGender === 'boy',
+                                'bg-white text-gray-300': filterForGender !== 'boy'
+                            }
+                        )
+                }>Niño</div>
+                <div 
+                    onClick={() => OnSelect('unisex')}
+                    className={
+                        clsx(
+                            "text-center rounded-md w-[100px] p-1",
+                            { 
+                                'bg-amber-600 text-white': filterForGender === 'unisex',
+                                'bg-white text-gray-300': filterForGender !== 'unisex'
+                            }
+                        )
+                }>Unisex</div>
+            </section>
+            <div className="px-3">
+                <h1 className="text-gray-400 font-bold">{rangesOfSizes[filterForGender]}</h1>
+            </div>
+        </>
     )
 }
